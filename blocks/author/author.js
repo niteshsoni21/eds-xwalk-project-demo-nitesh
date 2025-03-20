121f10e55d5227dc84b57bd54030ea1f05171218
sravanikancharla <sravanikancharla@bsci.com>
2025-03-20 17:36:10 +0530

    updated Js

--- blocks/author/author.js
+++ blocks/author/author.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
   const blockAuthor = document.createElement('div');
   [...block.children].forEach((row) => {
-  }
-  );
+    console.log(row.innerHTML); 
+  });
+  blockAuthor.innerHTML = block.innerHTML;
 }
