fc1de7c04906870074df52fdda90c54736ee56fa
Ravi Pandey <ravi.pandey01@nagarro.com>
2025-03-21 11:16:42 +0530

    video block JS fix

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,22 +1,22 @@
 export default function decorate(block) {
-    const videoUrl = block.querySelector(":scope > div > div");
+  const videoUrl = block.querySelector(':scope > div > div');
 
-    const videoContainer = document.createElement('div');
-    videoContainer.classList.add("video-container");
+  const videoContainer = document.createElement('div');
+  videoContainer.classList.add('video-container');
 
-    const video = document.createElement('video');
-    video.muted = true;
-    video.toggleAttribute('autoplay',true);
-    video.toggleAttribute('controls', true);
-    video.toggleAttribute('loop', true);
-    video.toggleAttribute('playsinline', true);
-    const source = document.createElement('source');
-    source.setAttribute('src', videoUrl.innerText);
-    source.setAttribute('type', 'video/mp4');
-    video.append(source);
+  const video = document.createElement('video');
+  video.muted = true;
+  video.toggleAttribute('autoplay', true);
+  video.toggleAttribute('controls', true);
+  video.toggleAttribute('loop', true);
+  video.toggleAttribute('playsinline', true);
+  const source = document.createElement('source');
+  source.setAttribute('src', videoUrl.innerText);
+  source.setAttribute('type', 'video/mp4');
+  video.append(source);
 
-    videoContainer.append(video);
+  videoContainer.append(video);
 
-    block.innerHTML = '';
-    block.appendChild(videoContainer);
+  block.innerHTML = '';
+  block.appendChild(videoContainer);
 }
