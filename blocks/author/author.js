9b2f1e02463a8ddedeea0dad5fadf7e322a6ddde
sravanikancharla <sravanikancharla@bsci.com>
2025-03-21 17:13:45 +0530

    updated author.js

--- blocks/author/author.js
+++ blocks/author/author.js
@@ -1,7 +1,26 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
   const blockAuthor = document.createElement('div');
-  [...block.children].forEach((row) => {
-    row.innerHTML = blockAuthor.innerHTML;
+  block.querySelectorAll('[data-aue-prop]').forEach((ele) => {
+    const propClass = ele.getAttribute('data-aue-prop').split('_')[1];
+    ele.classList.add(propClass);
   });
-  blockAuthor.innerHTML = block.innerHTML;
+
+  const ul = document.createElement('ul');
+  let socialDiv = false;
+  [...block.children].forEach((row) => {
+    row.querySelectorAll('[data-aue-prop]').forEach((ele) => {
+      if (ele.getAttribute('data-aue-prop').startsWith('social')) {
+        socialDiv = true;
+        const li = document.createElement('li');
+        moveInstrumentation(ele, li);
+        ul.append(li);
+        blockAuthor.append(ul);
+      }
+      if (socialDiv) {
+        row.innerHTML = blockAuthor.innerHTML;
+      }
+    })
+  });
 }
