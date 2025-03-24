c59e3a154c350f150e070602759e84d11b4cd6dd
Ravi Pandey <ravi.pandey01@nagarro.com>
2025-03-24 18:38:42 +0530

    recommended-variants updates

--- blocks/recommended-variants/recommended-variants.js
+++ blocks/recommended-variants/recommended-variants.js
@@ -1,48 +1,46 @@
 export default async function decorate(block) {
   block.classList.add('variant-details');
 
-  try {
-    const response = await fetch(`/graphql/execute.json/eds-nitesh-demo/carVariantList`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+  const response = await fetch('/graphql/execute.json/eds-nitesh-demo/carVariantList', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
 
-    const carVariantList = await response.json();
-
-    carVariantList.data.carVariantList.items.forEach(variant => {
-      const variantDiv = document.createElement('div');
-      variantDiv.classList.add('variant-card');
-
-      variantDiv.innerHTML = `
-        <div class="card-top">
-          <img src='${variant.variantImage._path}' alt='${variant.variantTitle}' class='variant-img' />
-          <div class='variant-share-section'>
-            <p class='variant-share-text'>Share</p>
-            <div class='top-selling-container ${variant.topSelling}'>
-              <p class='top-selling-text'>Top Selling</p>
-            </div>
-          </div>
-        </div>
-        <div class='variant-desc'>
-          <p class='variant-title'>${variant.variantTitle}</p>
-          <div class="variant-price-section">
-            <p class='variant-price'>${variant.variantPrice}</p>
-            <p class='variant-price-text'>${variant.variantPriceLocation}</p>
-          </div>
-          <p class="variant-feature">Features</p>
-          ${variant.variantFeatures.html}
-        </div>
-      `;
-
-      block.appendChild(variantDiv);
-    });
-  } catch (error) {
-    console.error("Error fetching car variants:", error);
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
   }
+
+  const carVariantList = await response.json();
+
+  carVariantList.data.carVariantList.items.forEach((variant) => {
+    const variantDiv = document.createElement('div');
+    variantDiv.classList.add('variant-card');
+
+    const imagePath = Object.values(variant.variantImage)[0];      variantDiv.innerHTML = `
+
+    variantDiv.innerHTML = `
+      <div class="card-top">
+        <img src='${imagePath}' alt='${variant.variantTitle}' class='variant-img' />
+        <div class='variant-share-section'>
+          <p class='variant-share-text'>Share</p>
+          <div class='top-selling-container ${variant.topSelling}'>
+            <p class='top-selling-text'>Top Selling</p>
+          </div>
+        </div>
+      </div>
+      <div class='variant-desc'>
+        <p class='variant-title'>${variant.variantTitle}</p>
+        <div class="variant-price-section">
+          <p class='variant-price'>${variant.variantPrice}</p>
+          <p class='variant-price-text'>${variant.variantPriceLocation}</p>
+        </div>
+        <p class="variant-feature">Features</p>
+        ${variant.variantFeatures.html}
+      </div>
+    `;
+
+    block.appendChild(variantDiv);
+  });
 }
