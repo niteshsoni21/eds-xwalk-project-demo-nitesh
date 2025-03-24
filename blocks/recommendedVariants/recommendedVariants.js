1f0246c2c8d0c7bc0574b1d215c6f4e681fe7a96
Ravi Pandey <ravi.pandey01@nagarro.com>
2025-03-24 15:11:23 +0530

    recommendedVariants updates

--- blocks/recommendedVariants/recommendedVariants.js
+++ blocks/recommendedVariants/recommendedVariants.js
@@ -1,37 +1,16 @@
 export default function decorate(block) {
   const variantsContainer = block.querySelector(':scope > div > div');
+  const variantDiv = document.createElement('div');
+    variantDiv.classList.add('car-variant');
 
-  const carVariantList = {
-      'items': [
-          {
-              'modelId': 'maruti-brezza-lxi-cng',
-              'variantImage': {
-                  '_path': '/content/dam/demo-auto/english/images/highlights/Highlight-01.webp'
-              },
-              'variantTitle': 'Maruti Brezza Lxi CNG',
-              'variantPrice': '₹ 10,00,000',
-              'variantPriceLocation': 'Ex-showroom, Punjab',
-              'variantFeatures': {
-                  'html': '<ul>\n<li>Safety First</li>\n<li>Lorem Ipsum Daller Sit</li>\n<li>Steel Wheels With Wheel Cover</li>\n<li>Projector Headlamps</li>\n<li>Chrome Accentuated Front Grille</li>\n</ul>'
-              }
-          }
-      ]
-  };
-
-  carVariantList.items.forEach(variant => {
-      const variantDiv = document.createElement('div');
-      variantDiv.classList.add('car-variant');
-
-      variantDiv.innerHTML = `
-          <div class='variant'>
-              <img src='${variant.variantImage._path}' alt='${variant.variantTitle}' class='variant-image' />
-              <h2 class='variant-title'>${variant.variantTitle}</h2>
-              <p class='variant-price'>${variant.variantPrice}</p>
-              <p class='variant-location'>${variant.variantPriceLocation}</p>
-              <div class='variant-features'>${variant.variantFeatures.html}</div>
-          </div>
-      `;
-
-      variantsContainer.appendChild(variantDiv);
-  });
+    variantDiv.innerHTML = `
+      <div class='variant'>
+        <img src='/content/dam/demo-auto/english/images/highlights/Highlight-01.webp' alt='Maruti Brezza Lxi CNG' class='variant-image' />
+        <h2 class='variant-title'>Maruti Brezza Lxi CNG</h2>
+        <p class='variant-price'>₹ 10,00,000</p>
+        <p class='variant-location'>Ex-showroom, Punjab</p>
+        <div class='variant-features'><ul>\n<li>Safety First</li>\n<li>Lorem Ipsum Daller Sit</li>\n<li>Steel Wheels With Wheel Cover</li>\n<li>Projector Headlamps</li>\n<li>Chrome Accentuated Front Grille</li>\n</ul></div>
+      </div>
+    `;
+    variantsContainer.appendChild(variantDiv);
 }
