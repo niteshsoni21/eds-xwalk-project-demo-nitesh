51c58f8a6b41f2983bbefbb2fca9103f2bb8fdd5
Ravi Pandey <ravi.pandey01@nagarro.com>
2025-03-24 17:57:07 +0530

    recommended-variants updates

--- blocks/recommended-variants/recommended-variants.js
+++ blocks/recommended-variants/recommended-variants.js
@@ -1,20 +1,48 @@
-export default function decorate(block) {
-  block.classList.add('variants-wrapper');
+export default async function decorate(block) {
+  block.classList.add('variant-details');
 
-  const variantDiv = document.createElement('div');
-  variantDiv.classList.add('car-variant');
-  console.log('car-variant class added');
+  try {
+    const response = await fetch(`/graphql/execute.json/eds-nitesh-demo/carVariantList`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
-  variantDiv.innerHTML = `
-    <div class='variant'>
-      <img src='/content/dam/demo-auto/english/images/highlights/Highlight-01.webp' alt='Maruti Brezza Lxi CNG' class='variant-image' />
-      <h2 class='variant-title'>Maruti Brezza Lxi CNG</h2>
-      <p class='variant-price'>₹ 10,00,000</p>
-      <p class='variant-location'>Ex-showroom, Punjab</p>
-      <div class='variant-features'><ul>\n<li>Safety First</li>\n<li>Lorem Ipsum Daller Sit</li>\n<li>Steel Wheels With Wheel Cover</li>\n<li>Projector Headlamps</li>\n<li>Chrome Accentuated Front Grille</li>\n</ul></div>
-    </div>
-  `;
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
 
-  block.appendChild(variantDiv);
-  console.log('variantDiv added');
+    const carVariantList = await response.json();
+
+    carVariantList.data.carVariantList.items.forEach(variant => {
+      const variantDiv = document.createElement('div');
+      variantDiv.classList.add('variant-card');
+
+      variantDiv.innerHTML = `
+        <div class="card-top">
+          <img src='${variant.variantImage._path}' alt='${variant.variantTitle}' class='variant-img' />
+          <div class='variant-share-section'>
+            <p class='variant-share-text'>Share</p>
+            <div class='top-selling-container'>
+              <p class='top-selling-text ${variant.topSelling}'>Top Selling</p>
+            </div>
+          </div>
+        </div>
+        <div class='variant-desc'>
+          <p class='variant-title'>${variant.variantTitle}</p>
+          <div class="variant-price-section">
+            <p class='variant-price'>${variant.variantPrice}</p>
+            <p class='variant-price-text'>${variant.variantPriceLocation}</p>
+          </div>
+          <p class="variant-feature">Features</p>
+          ${variant.variantFeatures.html}
+        </div>
+      `;
+
+      block.appendChild(variantDiv);
+    });
+  } catch (error) {
+    console.error("Error fetching car variants:", error);
+  }
 }
