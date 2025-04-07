6c99e84a6c85104e53054d076fc11b0710eb6a72
sravanikancharla <sravanikancharla@bsci.com>
2025-04-07 19:37:37 +0530

    updated tags.js

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -1,26 +1,34 @@
+function updateTagsList(ele, ul, ulClasses) {
+  const tags = ele.innerHTML.split(',');
+  ul.classList.add(ulClasses[i]);
+  ul.id = ulClasses[i];
+  tags.forEach((tag) => {
+    const li = document.createElement('li');
+    li.innerHTML = tag;
+    ul.append(li);
+  });
+}
+
 export default function decorate(block) {
   const tagClasses = ['tagsblock-tags', 'tagsblock-suggestedfeatures'];
   const ulClasses = ['tags-list', 'suggestedfeatures-list'];
   [...block.children].forEach((row, i) => {
     const tagsBlock = document.createElement('div');
     row.classList.add(tagClasses[i]);
-    row.querySelectorAll('div>div').forEach((ele) => {
+    const ul = document.createElement('ul');
+    let length = row.querySelectorAll('p').length;
+    if (length < 2) {
+      row.querySelectorAll('p').forEach((ele) => {
+        updateTagsList(ele, ul, ulClasses);
+      });
+    } else {
       const titleDiv = document.createElement('div');
       titleDiv.classList.add('tags-title');
-      titleDiv.innerHTML = ele.innerHTML;
+      titleDiv.innerHTML = row.querySelectorAll('p')[0].innerHTML;
       tagsBlock.append(titleDiv);
-    });
-    const ul = document.createElement('ul');
-    row.querySelectorAll('div>p').forEach((ele) => {
-      const tags = ele.innerHTML.split(',');
-      ul.classList.add(ulClasses[i]);
-      ul.id = ulClasses[i];
-      tags.forEach((tag) => {
-        const li = document.createElement('li');
-        li.innerHTML = tag;
-        ul.append(li);
-      });
-      tagsBlock.append(ul);
+      updateTagsList(row.querySelectorAll('p')[1], ul, ulClasses);
+    }
+    tagsBlock.append(ul);
     });
     row.innerHTML = tagsBlock.innerHTML;
   });
